882e8819a1c8f0f6d455bf98a3947f4f74e37116
Giuseppe Pasculli <35472996+joosefupas@users.noreply.github.com>
2023-04-13 11:57:19 +0200

    Update SpotiSave.R

--- SpotiSave.R
+++ SpotiSave.R
@@ -1,6 +1,6 @@
+# load libraries 
 library(spotifyr)
 library(dplyr)
-remove(list=ls())
 
 # Spotify API credentials
 Sys.setenv(SPOTIFY_CLIENT_ID = 'your_client_id_here')
@@ -10,12 +10,12 @@ redirect_uri = "http://localhost/"
 # put your(s) username(s) you want to save (if not yours/i.e. other users will save just the public playlists)
 usernames = c("username1", "username2")
 
+
 # Get an access token
 access_token <- get_spotify_access_token()
 
-# Loop through each user's playlists and get the track data
 for (username in usernames){
-
+  
   # Get the user's playlists
   playlists <- list()
   offset <- 0
@@ -29,47 +29,92 @@ for (username in usernames){
     playlists <- rbind(playlists, new_playlists)
     offset <- offset + limit
   }
-
+  
+  
+  # Group by 'owner.display_name' and count the number of occurrences
+  counts <- table(playlists$owner.display_name)
+  
+  # Get the most common value
+  most_common <- names(counts)[which.max(counts)]
+  
+  # Filter the dataset by the most common value
+  playlists <- playlists[playlists$owner.display_name == most_common, ]
+  
+  
   # Create a data frame to store the tracklists
-  track_data <- data.frame(Playlist = character(),
-                           Playlist_ID = character(),
-                           Track_ID = character(),
-                           Track_Name = character(),
-                           Artist_Name = character(),
-                           stringsAsFactors = FALSE)
-
-  if (length(playlists$id) > 0) {
-    for (i in 1:length(playlists$id)) {
-      tracks <- get_playlist_tracks(playlists$id[i])
-      if (length(tracks$track.id) > 0) {
-        for (j in 1:length(tracks$track.id)) {
-          track_data <- rbind(track_data, data.frame(Playlist = playlists$name[[i]],
-                                                     Playlist_ID = playlists$id[[i]],
-                                                     Owner = playlists$owner.display_name[[i]],
-                                                     Track_ID = tracks$track.id[[j]],
-                                                     Track_Name = tracks$track.name[[j]],
-                                                     Artist_Name = tracks$track.artists[[j]]$name,
-                                                     stringsAsFactors = FALSE))
-        }
+  
+  tracks = list()
+  playlist_ids = playlists$id
+  playlist_vector = c()
+  
+  # Loop through each playlist id and retrieve all tracks, with a maximum of 50 at a time
+  for (playlist_id in playlist_ids){  
+    offset = 0
+    limit = 50
+    total_tracks = playlists %>% filter(id==playlist_id) %>% pull(tracks.total)
+    
+    if (ncol(get_playlist_tracks(playlist_id=playlist_id)) ==42){
+      while (offset <= total_tracks){
+        tryCatch({
+          new_tracks = get_playlist_tracks(playlist_id = playlist_id, offset = offset, limit = limit)
+          offset = offset + limit
+          tracks = rbind(tracks, new_tracks)
+        }, error = function(e){ # Catch the error and display error message
+          message(paste0("Error occurred: ", e$message))
+        })
       }
+      playlist_vector_temp = rep(playlist_id,total_tracks)
+      playlist_vector = c(playlist_vector,playlist_vector_temp)  
+    }
+    
+    
+    
+  }
+  
+  # Bind playlist id to each track
+  tracks = cbind(tracks,playlist_vector)
+  tracks = tracks %>% dplyr::rename(id = playlist_vector)
+  
+  # Remove any rows where the track id is missing
+  tracks_temp = tracks
+  
+  # clean 
+  for (i in 1:nrow(tracks_temp)) {
+    if (is.na(tracks_temp[i, "track.id"])) {
+      tracks_temp <- tracks_temp[-i,]
     }
   }
-
-  # Filter the data to only include tracks from the most common playlist owner
-  top_display_name <- track_data %>%
-    dplyr::group_by(Owner) %>%
-    summarize(count = n()) %>%
-    top_n(1, count) %>%
-    pull(Owner)
-
-  filtered_track_data <- track_data %>%
-    filter(Owner %in% top_display_name) %>%
-    group_by(Playlist_ID, Track_ID) %>%
-    summarize(Playlist = first(Playlist),
-              Track_Name = first(Track_Name),
-              Artist_Name = paste(Artist_Name, collapse = ",")) %>%
-    ungroup()
-
-  # Write the data to a CSV file
-  write.csv(filtered_track_data, file = paste("track_data_",username,".csv",sep=""), row.names = FALSE)
+  
+  # Retrieve the name(s) of each artist associated with each track
+  artist_names = c()
+  
+  for (i in 1:length(tracks_temp$track.artists)){
+    if (!is.null(tracks_temp$track.artists[[i]])){
+      tryCatch({
+        if (length(data.frame(tracks_temp$track.artists[i]) %>% pull(name)) == 1){
+          temp_artist_name = data.frame(tracks_temp$track.artists[i]) %>% pull(name)
+          artist_names = c(artist_names, temp_artist_name)
+        } else {
+          temp_artist_name = paste(tracks_temp$track.artists[[i]]$name, collapse = ", ")
+          artist_names = c(artist_names, temp_artist_name)
+        }
+      },
+      error = function(e) {
+        print(paste("Error occurred for track artist", i, ":", e))
+      })
+    }
+  }
+  
+  # Combine track and playlist data into one dataframe
+  tracks_comp = left_join(tracks_temp,playlists %>% dplyr::select(id,name,owner.display_name))
+  tracks_comp_fin = tracks_comp %>% dplyr::select(id,owner.display_name,name,track.id,track.name)
+  tracks_comp_fin = cbind(tracks_comp_fin,artist_names) %>% dplyr::select(id,owner.display_name,name, artist_names, track.name,track.id) %>%  
+    dplyr::rename(playlist_id = id, artist_name = artist_names, playlist_name=name)
+  
+  
+  # Write data to CSV file
+  write.csv(tracks_comp_fin, file = paste("track_data_",username,"_",Sys.Date(),".csv",sep=""), row.names = FALSE)
 }
+
+
+
